36cf0259685acb89dfe6e41d37fcbe9f28807ba1
name1116 <milk100409@gmail.com>
2024-11-04 14:32:21 +0900

    About us 작업

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@
 $(document).ready(function(){
   console.log('hello')
     //헤더 푸터 컴포넌트
-    $('#header').load('../components/header.html');
-    $('#footer').load('../components/footer.html');
+    $('#header').load('./components/header.html');
+    $('#footer').load('./components/footer.html');
 
-    $('#pagination').load('../components/pagination.html');
-    $('#top_menu').load('../components/top_menu.html');
+    $('#pagination').load('./components/pagination.html');
+    $('#top_menu').load('./components/top_menu.html');
 })
 
 // /*smooth-scroll*/
